9532c949914d74e5363b8f21ba9d0f217635b738
Pavel <itztoru@vk.com>
2023-08-05 02:25:49 +0600

    Added sharing

--- vkbot.py
+++ vkbot.py
@@ -1,16 +1,12 @@
-import json
 from pprint import pprint
 import os
 
-from vkbottle.bot import Message
-from vkbottle import Bot, API
-from vkbottle import Text, BaseStateGroup
+from vkbottle.bot import Bot, Message
 import sqlite3
 
 from consts import admins
 
 bot = Bot(os.getenv('VKTOKEN'))
-api = bot.api
 
 database = sqlite3.connect("database.db")
 
@@ -20,38 +16,47 @@ db_cursor.execute('CREATE TABLE IF NOT EXISTS student_groups (id INT, course INT
 
 database.commit()
 
-@bot.on.chat_message(text='Рассылка: <type_of_message>')
-async def sharing(message: Message, type_of_message: str):
+
+async def share_messages(courses: list, text=None, attachment=None):
+    for course in courses:
+        groups = list(db_cursor.execute(f"SELECT id FROM student_groups WHERE course == {course}"))
+        for group in groups:
+            await bot.api.messages.send(
+                peer_id=(int(2e9) + group[0]),
+                message=text,
+                attachment=attachment,
+                random_id=0
+            )
+
+
+@bot.on.chat_message(text='Рассылка: <courses>, <share_type>')
+async def sharing(message: Message, courses: str, share_type: str):
     if message.from_id not in admins:
         await message.answer('Ваш id {' + str(message.from_id) + '} не является администратором')
         return
-    message.answer(message.group_id)
-    if type_of_message == 'Пост':
-        attachments = message.get_wall_attachment()
-        post_id = attachments[0].id
-        owner_id = attachments[0].owner_id
-        forward_txt = f"wall{owner_id}_{post_id}"
-        await message.answer(attachment=[forward_txt])
-    elif type_of_message == 'Сообщение':
+
+    courses_list = courses.split()
+
+    if share_type == 'Пост':
+        attachment = message.get_wall_attachment()[0]
+        forward_txt = f"wall{attachment.owner_id}_{attachment.id}"
+        await share_messages(courses_list, attachment=[forward_txt])
+    elif share_type == 'Сообщение':
         if message.fwd_messages:
-            await message.answer(message.fwd_messages[0].text)
+            await share_messages(courses_list, text=message.fwd_messages[0].text)
         else:
             await message.answer('Ошибка: нет пересланного сообщения')
     else:
         await message.answer('Ошибка: Не верно указан тип')
 
-@bot.on.chat_message(text='Текст: <text>')
-async def sharing_text(message: Message, text: str):
-    if message.from_id not in admins:
-        await message.answer('Ваш id {' + str(message.from_id) + '} не является администратором')
-        return
-    message.answer(message.group_id)
-    message.answer(text)
 
+@bot.on.chat_message(text='Рассылка: <courses>, Текст <text>')
+async def sharing_text(message: Message, text: str):
+    return
 @bot.on.chat_message(text='Добавить <course>')
 async def test(message: Message, course):
     group_id = message.peer_id - int(2e9)
-    members_cnt = (await api.messages.get_conversation_members(peer_id=(message.peer_id - int(2e9)))).count
+    members_cnt = (await bot.api.messages.get_conversation_members(peer_id=(message.peer_id - int(2e9)))).count
 
     course_ids = list(db_cursor.execute(f"SELECT id FROM student_groups"))
 
@@ -77,5 +82,8 @@ async def test(message: Message, course):
     # db_cursor.execute('INSERT INTO first_course (id, peoples, name, status) VALUES (0, 2, "name", "ok")')
     # database.commit()
 
+@bot.on.chat_message()
+async def sharing_text(message: Message):
+    await message.answer('Неизвестная команда')
 
 bot.run_forever()
