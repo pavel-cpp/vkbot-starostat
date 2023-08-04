9cae6535d61fa1b771be04a1b978f0f102c03e6b
Pavel <itztoru@vk.com>
2023-08-05 01:16:45 +0600

    -renamed main.py -> vkbot.py -added database -added registration system (Beta)

--- vkbot.py
+++ vkbot.py
@@ -2,12 +2,23 @@ import json
 from pprint import pprint
 import os
 
-from vkbottle.bot import Bot, Message
+from vkbottle.bot import Message
+from vkbottle import Bot, API
 from vkbottle import Text, BaseStateGroup
+import sqlite3
 
 from consts import admins
 
 bot = Bot(os.getenv('VKTOKEN'))
+api = bot.api
+
+database = sqlite3.connect("database.db")
+
+db_cursor = database.cursor()
+
+db_cursor.execute('CREATE TABLE IF NOT EXISTS student_groups (id INT, course INT, members_count INT)')
+
+database.commit()
 
 @bot.on.chat_message(text='Рассылка: <type_of_message>')
 async def sharing(message: Message, type_of_message: str):
@@ -37,7 +48,34 @@ async def sharing_text(message: Message, text: str):
     message.answer(message.group_id)
     message.answer(text)
 
+@bot.on.chat_message(text='Добавить <course>')
+async def test(message: Message, course):
+    group_id = message.peer_id - int(2e9)
+    members_cnt = (await api.messages.get_conversation_members(peer_id=(message.peer_id - int(2e9)))).count
 
+    course_ids = list(db_cursor.execute(f"SELECT id FROM student_groups"))
+
+    pprint(course_ids)
+
+    if course != 'admin':
+        if int(course) < 0 or int(course) > 5:
+            await message.answer("Такого курса не существует!")
+    else:
+        course = '-1'
+
+    pprint(f"INSERT INTO student_groups VALUES ({group_id}, {course}, {members_cnt})")
+
+    for i in course_ids:
+        if i[0] == group_id:
+            await message.answer('Ваша беседа уже есть в списке')
+            return
+
+    db_cursor.execute(f"INSERT INTO student_groups VALUES ({group_id}, {course}, {members_cnt})")
+    database.commit()
+    await message.answer('Ваша беседа успешно добавлена')
+
+    # db_cursor.execute('INSERT INTO first_course (id, peoples, name, status) VALUES (0, 2, "name", "ok")')
+    # database.commit()
 
 
 bot.run_forever()
